fb501ba29540441159605385d22f36b25fd3c6fe
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2024-01-13 11:15:20 -0300

    Add/Remove Item from Order Table

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,7 +11,9 @@ import { CreateProductController } from "./controllers/products/CreateProductCon
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 import uploadConfig from "./config/multer";
+import { AddItemController } from "./controllers/order/AddItemController";
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { ListByCategoryController } from "./controllers/products/ListByCategoryController";
 
@@ -52,5 +54,13 @@ router.get(
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
+
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
+
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 
 export { router };
