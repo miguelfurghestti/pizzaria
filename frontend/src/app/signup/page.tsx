edd42d2e1168366a38a4ed44dd18e37d29a51d1f
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2024-01-14 08:55:40 -0300

    Frontend, first configs, login screen, register screen

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,33 +1,34 @@
 import Image from "next/image";
-import logoImg from "../../public/logo.svg";
-import styles from "../../styles/home.module.scss";
+import logoImg from "../../../public/logo.svg";
+import styles from "../../../styles/home.module.scss";
 
-import { Button } from "../../src/components/ui/Button";
-import { Input } from "../../src/components/ui/Input";
+import { Button } from "../../components/ui/Button";
+import { Input } from "../../components/ui/Input";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
-          <h1>Acesse sua conta</h1>
-
+          <h1>Criando a sua conta</h1>
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu e-mail" type="text" />
 
             <Input placeholder="Digite sua senha" type="password" />
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link className={styles.text} href="/signup">
-            Não possui uma conta? <strong>Cadastre-se</strong>
+          <Link className={styles.text} href="/">
+            Já possuo uma conta. <strong>Entrar</strong>
           </Link>
         </div>
       </div>
---
+++ frontend/src/components/ui/Input/index.tsx
@@ -0,0 +1,13 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import styles from "./styles.module.scss";
+
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input({ ...rest }: InputProps) {
+  return <input className={styles.input} {...rest} />;
+}
+
+export function TextArea({ ...rest }: TextAreaProps) {
+  return <textarea className={styles.input} {...rest}></textarea>;
+}
